e0e4d1977ab7ae34e5833e43903057c5db9982f0
Christian Stein <stein.robotics@gmail.com>
2026-09-08 11:48:05 +0200

    fix assembly_leg.py

--- nikita_doc/3D/assembly_leg.py
+++ nikita_doc/3D/assembly_leg.py
@@ -26,15 +26,20 @@ def build_assembly() -> Compound:
     servo.color = COLOR_DARK_GRAY
     coxa_assembly = assembly_coxa.build_assembly()
     femur_assembly = assembly_femur.build_assembly()
-    # foot_servo_assembly = assembly_foot_servo.build_assembly()
+    foot_servo_assembly = assembly_foot_servo.build_assembly()
 
+    ##########################################################
+    ## Connect servo to coxa assembly
+    ##########################################################    
     servo.joints["rotation"].connect_to(
         coxa_assembly.joints["joint_coxa"],
         angle=ANGLE_COXA,
     )
     servo = Pos(0, BRACKET_Y_OFFSET, 0) * servo
 
-    
+    ##########################################################
+    ## Connect coxa assembly to femur assembly
+    ##########################################################
     coxa_assembly.joints["joint_femur"].connect_to(
         femur_assembly.joints["horn"],
         angle=ANGLE_FEMUR,
@@ -43,7 +48,20 @@ def build_assembly() -> Compound:
     p = coxa_assembly.joints["joint_femur"].location.position
     femur_assembly = Pos(p.X, p.Y, p.Z) * Rot(60 + ANGLE_FEMUR, 0, 180) * Pos(p.X + BRACKET_Y_OFFSET, -p.Y, -p.Z) * femur_assembly
 
-    leg = Compound(children=[servo, coxa_assembly, femur_assembly])
+    ##########################################################
+    ## Connect femur assembly to foot servo assembly
+    ##########################################################
+    femur_assembly.joints["servo_attachment"].connect_to(
+        foot_servo_assembly.joints["horn"],
+        angle=ANGLE_TIBIA,
+    )
+    # Rotate around the joint connection point, not the world origin
+    p = femur_assembly.joints["servo_attachment"].location.position
+    foot_servo_assembly = Pos(p.X, p.Y, p.Z) * Rot(ANGLE_TIBIA , 0, 180) * Pos(p.X - BRACKET_Y_OFFSET, -p.Y, -p.Z) * foot_servo_assembly
+
+
+
+    leg = Compound(children=[servo, coxa_assembly, femur_assembly, foot_servo_assembly])
     return leg
 
 def main() -> None:
